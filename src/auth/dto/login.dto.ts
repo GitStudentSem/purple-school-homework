ff336f989b324ae71e0c1350c1dba4f017eaf16e
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2025-03-10 18:47:49 +0300

    tests for login

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,13 +2,14 @@ import { IsEmail, IsString, Length } from "class-validator";
 import {
 	INCORRECT_EMAIL_FORMAT,
 	INCORRECT_PASSWORD_LENGTH,
+	INCORRECT_PASSWORD_TYPE,
 } from "../auth.constants";
 
 export class LoginDto {
 	@IsEmail(undefined, { message: INCORRECT_EMAIL_FORMAT })
 	email: string;
 
-	@IsString({ message: INCORRECT_PASSWORD_LENGTH })
+	@IsString({ message: INCORRECT_PASSWORD_TYPE })
 	@Length(6, undefined, { message: INCORRECT_PASSWORD_LENGTH })
 	password: string;
 }
---
+++ test/auth/auth.login.e2e-spec.ts
@@ -0,0 +1,90 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { App } from "supertest/types";
+import { AppModule } from "../../src/app.module";
+import { disconnect } from "mongoose";
+import { LoginDto } from "../../src/auth/dto/login.dto";
+import {
+	INCORRECT_EMAIL_FORMAT,
+	INCORRECT_PASSWORD_LENGTH,
+	INCORRECT_PASSWORD_TYPE,
+	USER_NOT_FOUND_ERROR,
+} from "../../src/auth/auth.constants";
+
+const loginDto: LoginDto = {
+	email: "purnemtzev.semyon@yandex.ru",
+	password: "112233",
+};
+
+describe("/auth/login (POST)", () => {
+	let app: INestApplication<App>;
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
+
+	it("success", async () => {
+		return request(app.getHttpServer())
+			.post("/auth/login")
+			.send(loginDto)
+			.expect(201)
+			.then(({ body }: request.Response) => {
+				expect(body.access_token).toBeDefined();
+				return;
+			});
+	});
+
+	it("non-existent email", async () => {
+		return request(app.getHttpServer())
+			.post("/auth/login")
+			.send({ ...loginDto, email: "aaa@mail.ru" })
+			.expect(401)
+			.then(({ body }: request.Response) => {
+				expect(body.message).toBe(USER_NOT_FOUND_ERROR);
+				return;
+			});
+	});
+
+	it("incorrect email", async () => {
+		return request(app.getHttpServer())
+			.post("/auth/login")
+			.send({ ...loginDto, email: "aaa.ru" })
+			.expect(400)
+			.then(({ body }: request.Response) => {
+				expect(body.message[0]).toBe(INCORRECT_EMAIL_FORMAT);
+				return;
+			});
+	});
+
+	it("short password", async () => {
+		return request(app.getHttpServer())
+			.post("/auth/login")
+			.send({ ...loginDto, password: "123" })
+			.expect(400)
+			.then(({ body }: request.Response) => {
+				expect(body.message[0]).toBe(INCORRECT_PASSWORD_LENGTH);
+				return;
+			});
+	});
+
+	it("password is not a string", async () => {
+		return request(app.getHttpServer())
+			.post("/auth/login")
+			.send({ ...loginDto, password: 1234567 })
+			.expect(400)
+			.then(({ body }: request.Response) => {
+				expect(body.message).toContain(INCORRECT_PASSWORD_TYPE);
+				return;
+			});
+	});
+
+	afterAll(() => {
+		disconnect();
+	});
+});
